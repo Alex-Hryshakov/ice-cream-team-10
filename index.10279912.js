5501687d3b3fcf79d540bc3c2d27f1d95efdedab
Alex-Hryshakov <108174906+Alex-Hryshakov@users.noreply.github.com>
2022-09-06 15:22:11 +0000

    Deploying to gh-pages from @ Alex-Hryshakov/ice-cream-team-10@c1841c34b46f9d428df7ba86820a69d86bea9e9b 🚀

---
+++ index.10279912.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-read-modal-open]"),closeModalBtn:document.querySelector("[data-read-modal-close]"),modal:document.querySelector("[data-read-modal]")};function d(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",d),e.closeModalBtn.addEventListener("click",d)}();
+//# sourceMappingURL=index.10279912.js.map
